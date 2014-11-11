426fc491cd814842adb761b99ba6b0f81b28db01
Eduard Carreras <ecarreras@gisce.net>
2014-11-11 12:37:47 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='osconf',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/gisce/osconf',
+    license='MIT',
+    author='GISCE-TI, S.L.',
+    author_email='devel@gisce.net',
+    description='Configure from environment variables'
+)
