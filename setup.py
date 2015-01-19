8734fb4662a4bbf12f16947c8c4bc45feea19fe1
Eduard Carreras <ecarreras@gisce.net>
2015-01-19 10:58:16 +0100

    Bump to v0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='osconf',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     url='https://github.com/gisce/osconf',
     license='MIT',
